615c467dcf032fe403f2d4302087719eff786159
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-01-30 12:37:11 +0000

    Some fun spaceship physics

--- src/spaceship.coffee
+++ src/spaceship.coffee
@@ -7,7 +7,8 @@ class SpaceShip
     @x = size_x / 2
     @y = size_y / 2
     @angle = 0
-    @speed = 0
+    @dx = 0
+    @dy = 0
     @graphics = game.add.graphics(@x, @y)
     @graphics.lineStyle(0)
     @graphics.beginFill(0xFF0000)
@@ -20,25 +21,44 @@ class SpaceShip
     ])
     @graphics.endFill()
 
-  update: ->
-    dx =  Math.sin(Math.PI*2*@angle/360) * @speed
-    dy = -Math.cos(Math.PI*2*@angle/360) * @speed
-    @graphics.x += dx
-    @graphics.y += dy
+  ensure_bounds: ->
+    if @graphics.x < 20 and @dx < 0
+      @dx = -@dx
+    if @graphics.x > size_x-20 and @dx > 0
+      @dx = -@dx
+    if @graphics.y < 20 and @dy < 0
+      @dy = -@dy
+    if @graphics.y > size_y-20 and @dy > 0
+      @dy = -@dy
+
+  update: (dt) ->
+    @graphics.x += @dx * dt
+    @graphics.y += @dy * dt
     @graphics.angle = @angle
+    @ensure_bounds()
 
-  speed_up: (dt) ->
-    @speed += dt * 10.0
-    if @speed > 10.0
-      @speed = 10.0
+  dir_x: ->
+    Math.sin(Math.PI*2*@angle/360)
 
-  speed_down: (dt) ->
-    @speed -= dt * 10.0
-    if @speed < -10.0
-      @speed = -10.0
+  dir_y: ->
+    -Math.cos(Math.PI*2*@angle/360)
+
+  limit_speed: ->
+    dl = Math.sqrt(@dx*@dx + @dy*@dy)
+    max_speed = 250.0
+    if dl > max_speed
+      @dx *= (max_speed/dl)
+      @dy *= (max_speed/dl)
+
+  speed_up: (dir, dt) ->
+    speed_gain_per_second = 100.0
+    @dx += @dir_x() * dt * speed_gain_per_second * dir
+    @dy += @dir_y() * dt * speed_gain_per_second * dir
+    @limit_speed()
 
   turn: (dir, dt) ->
-    @angle += dir * dt * 100.0
+    degrees_per_second = 400.0
+    @angle += dir * dt * degrees_per_second
 
 class GameState
   constructor: ->
@@ -47,9 +67,9 @@ class GameState
   update: ->
     dt = @game.time.elapsed/1000.0
     if game.input.keyboard.isDown(Phaser.KeyCode.UP)
-      @space_ship.speed_up(dt)
+      @space_ship.speed_up(1.0, dt)
     if game.input.keyboard.isDown(Phaser.KeyCode.DOWN)
-      @space_ship.speed_down(dt)
+      @space_ship.speed_up(-1.0, dt)
     if game.input.keyboard.isDown(Phaser.KeyCode.LEFT)
       @space_ship.turn(-1.0, dt)
     if game.input.keyboard.isDown(Phaser.KeyCode.RIGHT)
--- src/spaceship.js
+++ src/spaceship.js
@@ -13,7 +13,8 @@
       this.x = size_x / 2;
       this.y = size_y / 2;
       this.angle = 0;
-      this.speed = 0;
+      this.dx = 0;
+      this.dy = 0;
       this.graphics = game.add.graphics(this.x, this.y);
       this.graphics.lineStyle(0);
       this.graphics.beginFill(0xFF0000);
@@ -21,31 +22,58 @@
       this.graphics.endFill();
     }
 
-    SpaceShip.prototype.update = function() {
-      var dx, dy;
-      dx = Math.sin(Math.PI * 2 * this.angle / 360) * this.speed;
-      dy = -Math.cos(Math.PI * 2 * this.angle / 360) * this.speed;
-      this.graphics.x += dx;
-      this.graphics.y += dy;
-      return this.graphics.angle = this.angle;
-    };
-
-    SpaceShip.prototype.speed_up = function(dt) {
-      this.speed += dt * 10.0;
-      if (this.speed > 10.0) {
-        return this.speed = 10.0;
+    SpaceShip.prototype.ensure_bounds = function() {
+      if (this.graphics.x < 20 && this.dx < 0) {
+        this.dx = -this.dx;
+      }
+      if (this.graphics.x > size_x - 20 && this.dx > 0) {
+        this.dx = -this.dx;
+      }
+      if (this.graphics.y < 20 && this.dy < 0) {
+        this.dy = -this.dy;
+      }
+      if (this.graphics.y > size_y - 20 && this.dy > 0) {
+        return this.dy = -this.dy;
       }
     };
 
-    SpaceShip.prototype.speed_down = function(dt) {
-      this.speed -= dt * 10.0;
-      if (this.speed < -10.0) {
-        return this.speed = -10.0;
+    SpaceShip.prototype.update = function(dt) {
+      this.graphics.x += this.dx * dt;
+      this.graphics.y += this.dy * dt;
+      this.graphics.angle = this.angle;
+      return this.ensure_bounds();
+    };
+
+    SpaceShip.prototype.dir_x = function() {
+      return Math.sin(Math.PI * 2 * this.angle / 360);
+    };
+
+    SpaceShip.prototype.dir_y = function() {
+      return -Math.cos(Math.PI * 2 * this.angle / 360);
+    };
+
+    SpaceShip.prototype.limit_speed = function() {
+      var dl, max_speed;
+      dl = Math.sqrt(this.dx * this.dx + this.dy * this.dy);
+      max_speed = 250.0;
+      if (dl > max_speed) {
+        this.dx *= max_speed / dl;
+        return this.dy *= max_speed / dl;
       }
+    };
+
+    SpaceShip.prototype.speed_up = function(dir, dt) {
+      var speed_gain_per_second;
+      speed_gain_per_second = 100.0;
+      this.dx += this.dir_x() * dt * speed_gain_per_second * dir;
+      this.dy += this.dir_y() * dt * speed_gain_per_second * dir;
+      return this.limit_speed();
     };
 
     SpaceShip.prototype.turn = function(dir, dt) {
-      return this.angle += dir * dt * 100.0;
+      var degrees_per_second;
+      degrees_per_second = 400.0;
+      return this.angle += dir * dt * degrees_per_second;
     };
 
     return SpaceShip;
@@ -61,10 +89,10 @@
       var dt;
       dt = this.game.time.elapsed / 1000.0;
       if (game.input.keyboard.isDown(Phaser.KeyCode.UP)) {
-        this.space_ship.speed_up(dt);
+        this.space_ship.speed_up(1.0, dt);
       }
       if (game.input.keyboard.isDown(Phaser.KeyCode.DOWN)) {
-        this.space_ship.speed_down(dt);
+        this.space_ship.speed_up(-1.0, dt);
       }
       if (game.input.keyboard.isDown(Phaser.KeyCode.LEFT)) {
         this.space_ship.turn(-1.0, dt);
