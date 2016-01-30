42af4b438622182105ab273c203d50523b3d1960
Anthony Low <anthony.low@xbridge.com>
2016-01-30 15:40:18 +0000

    pong working

--- src/pong.coffee
+++ src/pong.coffee
@@ -7,6 +7,7 @@ class GameState
     null
 
   update: ->
+    # move paddles up/down
     dt = @game.time.elapsed/1000.0
     if game.input.keyboard.isDown(Phaser.KeyCode.W)
       @left_paddle.y -= dt * 600
@@ -17,6 +18,53 @@ class GameState
       @right_paddle.y -= dt * 600
     if game.input.keyboard.isDown(Phaser.KeyCode.DOWN)
       @right_paddle.y += dt * 600
+
+    # ensure paddle boundaries
+    @left_paddle.y = game.math.clamp(@left_paddle.y, 75, size_y - 75)
+    @right_paddle.y = game.math.clamp(@right_paddle.y, 75, size_y - 75)
+
+    # move ball
+    @ball.x += dt * @ball_dx
+    @ball.y += dt * @ball_dy
+
+    # ball boundary check
+    @ensure_bounds()
+
+    # udpate scores
+    @left_score.text  = @left_score_val
+    @right_score.text = @right_score_val
+
+  ensure_bounds: ->
+    if @ball.x < 65 and @ball_dx < 0
+      if @hit_left_paddle()
+        @ball_dx *= -1.1
+        @ball_dy *= 1.1
+      else
+        @right_score_val += 1
+        @reset_ball()
+    if @ball.x > size_x-65 and @ball_dx > 0
+      if @hit_right_paddle()
+        @ball_dx *= -1.1
+        @ball_dy *= 1.1
+      else
+        @left_score_val += 1
+        @reset_ball()
+    if @ball.y < 25 and @ball_dy < 0
+      @ball_dy = -@ball_dy
+    if @ball.y > size_y-25 and @ball_dy > 0
+      @ball_dy = -@ball_dy
+
+  hit_left_paddle: ->
+    Math.abs(@left_paddle.y - @ball.y) < 65 + 25
+
+  hit_right_paddle: ->
+    Math.abs(@right_paddle.y - @ball.y) < 65 + 25
+
+  reset_ball: ->
+    @ball.x  = size_x / 2
+    @ball.y  = size_y / 2
+    @ball_dx = 150
+    @ball_dy = 150
 
   create: ->
     @game.stage.backgroundColor = "FFFF00"
@@ -29,10 +77,13 @@ class GameState
       @grid.lineTo(0, y + 10)
 
     # score display
-    @left_score = game.add.text(size_x / 4, size_y / 8, 'kittens', { fontSize: '100px', fill: '#000', align: "center" })
+    @left_score_val  = 0
+    @right_score_val = 0
+
+    @left_score = game.add.text(size_x / 4, size_y / 8, @left_score_val, { fontSize: '100px', fill: '#000', align: "center" })
     @left_score.anchor.set(0.5)
 
-    @right_score = game.add.text(size_x / 4 * 3, size_y / 8, 'puppies', { fontSize: '100px', fill: '#000', align: "center" })
+    @right_score = game.add.text(size_x / 4 * 3, size_y / 8, @right_score_val, { fontSize: '100px', fill: '#000', align: "center" })
     @right_score.anchor.set(0.5)
 
     # paddles
@@ -48,6 +99,13 @@ class GameState
     @right_paddle.beginFill(0x000)
     @right_paddle.drawRect(0, -65, 30, 130)
 
+    # ball
+    @ball = game.add.graphics(size_x / 2, size_y / 2)
+    @ball.lineStyle(0)
+    @ball.beginFill(0x000)
+    @ball.drawCircle(0, 0, 50)
+    @ball_dx = 300
+    @ball_dy = 300
 
 game = new Phaser.Game(size_x, size_y)
 game.state.add("Game", GameState, true)
--- src/pong.js
+++ src/pong.js
@@ -25,8 +25,57 @@ GameState = (function() {
       this.right_paddle.y -= dt * 600;
     }
     if (game.input.keyboard.isDown(Phaser.KeyCode.DOWN)) {
-      return this.right_paddle.y += dt * 600;
+      this.right_paddle.y += dt * 600;
     }
+    this.left_paddle.y = game.math.clamp(this.left_paddle.y, 75, size_y - 75);
+    this.right_paddle.y = game.math.clamp(this.right_paddle.y, 75, size_y - 75);
+    this.ball.x += dt * this.ball_dx;
+    this.ball.y += dt * this.ball_dy;
+    this.ensure_bounds();
+    this.left_score.text = this.left_score_val;
+    return this.right_score.text = this.right_score_val;
+  };
+
+  GameState.prototype.ensure_bounds = function() {
+    if (this.ball.x < 65 && this.ball_dx < 0) {
+      if (this.hit_left_paddle()) {
+        this.ball_dx *= -1.1;
+        this.ball_dy *= 1.1;
+      } else {
+        this.right_score_val += 1;
+        this.reset_ball();
+      }
+    }
+    if (this.ball.x > size_x - 65 && this.ball_dx > 0) {
+      if (this.hit_right_paddle()) {
+        this.ball_dx *= -1.1;
+        this.ball_dy *= 1.1;
+      } else {
+        this.left_score_val += 1;
+        this.reset_ball();
+      }
+    }
+    if (this.ball.y < 25 && this.ball_dy < 0) {
+      this.ball_dy = -this.ball_dy;
+    }
+    if (this.ball.y > size_y - 25 && this.ball_dy > 0) {
+      return this.ball_dy = -this.ball_dy;
+    }
+  };
+
+  GameState.prototype.hit_left_paddle = function() {
+    return Math.abs(this.left_paddle.y - this.ball.y) < 65 + 25;
+  };
+
+  GameState.prototype.hit_right_paddle = function() {
+    return Math.abs(this.right_paddle.y - this.ball.y) < 65 + 25;
+  };
+
+  GameState.prototype.reset_ball = function() {
+    this.ball.x = size_x / 2;
+    this.ball.y = size_y / 2;
+    this.ball_dx = 150;
+    return this.ball_dy = 150;
   };
 
   GameState.prototype.create = function() {
@@ -38,13 +87,15 @@ GameState = (function() {
       this.grid.moveTo(0, y);
       this.grid.lineTo(0, y + 10);
     }
-    this.left_score = game.add.text(size_x / 4, size_y / 8, 'kittens', {
+    this.left_score_val = 0;
+    this.right_score_val = 0;
+    this.left_score = game.add.text(size_x / 4, size_y / 8, this.left_score_val, {
       fontSize: '100px',
       fill: '#000',
       align: "center"
     });
     this.left_score.anchor.set(0.5);
-    this.right_score = game.add.text(size_x / 4 * 3, size_y / 8, 'puppies', {
+    this.right_score = game.add.text(size_x / 4 * 3, size_y / 8, this.right_score_val, {
       fontSize: '100px',
       fill: '#000',
       align: "center"
@@ -59,7 +110,13 @@ GameState = (function() {
     this.right_paddle.lineStyle(5, "white");
     this.right_paddle.lineStyle(0);
     this.right_paddle.beginFill(0x000);
-    return this.right_paddle.drawRect(0, -65, 30, 130);
+    this.right_paddle.drawRect(0, -65, 30, 130);
+    this.ball = game.add.graphics(size_x / 2, size_y / 2);
+    this.ball.lineStyle(0);
+    this.ball.beginFill(0x000);
+    this.ball.drawCircle(0, 0, 50);
+    this.ball_dx = 300;
+    return this.ball_dy = 300;
   };
 
   return GameState;
